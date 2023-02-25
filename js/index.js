bac2414d1783c2e1cb3cc7183412c9674c01f62e
Daniil <daniil.shest@gmail.com>
2023-02-25 13:16:56 +0600

    slider-test

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 100,
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    autoHeight: true
+});
