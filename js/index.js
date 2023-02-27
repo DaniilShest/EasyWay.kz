73e0aae6aeb7e738d564fbdbd22fd7f6e5283180
Daniil <daniil.shest@gmail.com>
2023-02-27 20:19:03 +0600

    main-page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.main-slider', {
     speed: 400,
     spaceBetween: 100,
     effect: 'fade',
@@ -6,9 +6,20 @@ const swiper = new Swiper('.swiper', {
         crossFade: true
     },
     pagination: {
-        el: '.swiper-pagination',
+        el: '.main-slider-pagination',
         type: 'bullets',
         clickable: true
     },
     autoHeight: true
+});
+
+const mainTrust = new Swiper('.main-trust__slider', {
+    speed: 400,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: '.main-trust-pagination',
+        type: 'bullets',
+        clickable: true
+    },
 });
