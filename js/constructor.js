f726e6d9fda97e8fad73b96316d870d47d223a64
Daniil <daniil.shest@gmail.com>
2023-03-08 11:50:23 +0600

    constructor

--- js/constructor.js
+++ js/constructor.js
@@ -3,9 +3,15 @@ const next = document.getElementById('item-constuctor-next')
 const prev = document.getElementById('item-constuctor-prev')
 const price = document.getElementById('item-constuctor-price')
 const submitBtn = document.getElementById('item-constructor-submit')
+const points = document.getElementById('item-constuctor-points')
+const line = document.getElementById('item-constuctor-line')
+const img = document.getElementById('item-constuctor-img')
 let activeStep = 0;
 const priceArray = {
     0: 440000
+}
+
+const pointsArray = {
 }
 
 
@@ -14,16 +20,29 @@ const formSteps = [
         title: 'Высота подъемника',
         options: [
             {
+                title: 'Высота до 1м.',
+                data: 0,
+                img: '../img/item/1.png'
+            },
+            {
+                title: 'Высота до 1,5м.',
+                data: 0,
+                img: '../img/item/2.png'
+            },
+            {
                 title: 'Высота до 2м.',
-                data: 0
+                data: 0,
+                img: '../img/item/3.png'
             },
             {
                 title: 'Высота до 2,5м.',
-                data: 50000
+                data: 50000,
+                img: '../img/item/4.png'
             },
             {
                 title: 'Высота до 3м.',
-                data: 50000
+                data: 50000,
+                img: '../img/item/5.png'
             }
         ]
     },
@@ -32,11 +51,13 @@ const formSteps = [
         options: [
             {
                 title: '1040 х 960 мм.',
-                data: 0
+                data: 0,
+                img: '../img/item/1.png'
             },
             {
                 title: '1250 х 960 мм.',
-                data: 10000
+                data: 10000,
+                img: '../img/item/2.png'
             }
         ]
     },
@@ -45,54 +66,324 @@ const formSteps = [
         options: [
             {
                 title: 'Перфорированный лист',
-                data: 0
+                data: 0,
+                img: '../img/item/1.png'
             },
             {
                 title: 'Алюминиевый рифленый лист',
-                data: 20000
+                data: 20000,
+                img: '../img/item/2.png'
+            }
+        ]
+    },
+    {
+        title: 'Скат с площадки подъемного механизма',
+        options: [
+            {
+                title: 'Cтальной (площадка с перфорированным листом)',
+                data: 0,
+                img: '../img/item/1.png'
+            },
+            {
+                title: 'Перфорированный (площадка с перфорированным листом)',
+                data: 0,
+                img: '../img/item/2.png'
+            },
+            {
+                title: 'Алюминиевый (площадка с алюминиевым рифленым листом)',
+                data: 0,
+                img: '../img/item/3.png'
+            },
+        ]
+    },
+    {
+        title: 'Заезд/выезд на площадку подъемного механизма',
+        options: [
+            {
+                title: 'Прямой',
+                data: 0,
+                img: '../img/item/1.png'
+            },
+            {
+                title: 'слева',
+                data: 0,
+                img: '../img/item/2.png'
+            },
+            {
+                title: 'справа',
+                data: 0,
+                img: '../img/item/3.png'
+            },
+            {
+                title: 'прямой и слева',
+                data: 0,
+                img: '../img/item/4.png'
+            },
+            {
+                title: 'прямой и справа',
+                data: 0,
+                img: '../img/item/5.png'
+            }
+        ]
+    },
+    {
+        title: 'Высота подъемника',
+        options: [
+            {
+                title: 'Высота до 1м.',
+                data: 0,
+                img: '../img/item/1.png'
+            },
+            {
+                title: 'Высота до 1,5м.',
+                data: 0,
+                img: '../img/item/2.png'
+            },
+            {
+                title: 'Высота до 2м.',
+                data: 0,
+                img: '../img/item/3.png'
+            },
+            {
+                title: 'Высота до 2,5м.',
+                data: 50000,
+                img: '../img/item/4.png'
+            },
+            {
+                title: 'Высота до 3м.',
+                data: 50000,
+                img: '../img/item/5.png'
+            }
+        ]
+    },
+    {
+        title: 'Площадка подъёмного механизма',
+        options: [
+            {
+                title: '1040 х 960 мм.',
+                data: 0,
+                img: '../img/item/1.png'
+            },
+            {
+                title: '1250 х 960 мм.',
+                data: 10000,
+                img: '../img/item/2.png'
+            }
+        ]
+    },
+    {
+        title: 'Покрытие площадки подъемного механизма',
+        options: [
+            {
+                title: 'Перфорированный лист',
+                data: 0,
+                img: '../img/item/1.png'
+            },
+            {
+                title: 'Алюминиевый рифленый лист',
+                data: 20000,
+                img: '../img/item/2.png'
+            }
+        ]
+    },
+    {
+        title: 'Скат с площадки подъемного механизма',
+        options: [
+            {
+                title: 'Cтальной (площадка с перфорированным листом)',
+                data: 0,
+                img: '../img/item/1.png'
+            },
+            {
+                title: 'Перфорированный (площадка с перфорированным листом)',
+                data: 0,
+                img: '../img/item/2.png'
+            },
+            {
+                title: 'Алюминиевый (площадка с алюминиевым рифленым листом)',
+                data: 0,
+                img: '../img/item/3.png'
+            },
+        ]
+    },
+    {
+        title: 'Заезд/выезд на площадку подъемного механизма',
+        options: [
+            {
+                title: 'Прямой',
+                data: 0,
+                img: '../img/item/1.png'
+            },
+            {
+                title: 'слева',
+                data: 0,
+                img: '../img/item/2.png'
+            },
+            {
+                title: 'справа',
+                data: 0,
+                img: '../img/item/3.png'
+            },
+            {
+                title: 'прямой и слева',
+                data: 0,
+                img: '../img/item/4.png'
+            },
+            {
+                title: 'прямой и справа',
+                data: 0,
+                img: '../img/item/5.png'
+            }
+        ]
+    }, {
+        title: 'Высота подъемника',
+        options: [
+            {
+                title: 'Высота до 1м.',
+                data: 0,
+                img: '../img/item/1.png'
+            },
+            {
+                title: 'Высота до 1,5м.',
+                data: 0,
+                img: '../img/item/2.png'
+            },
+            {
+                title: 'Высота до 2м.',
+                data: 0,
+                img: '../img/item/3.png'
+            },
+            {
+                title: 'Высота до 2,5м.',
+                data: 50000,
+                img: '../img/item/4.png'
+            },
+            {
+                title: 'Высота до 3м.',
+                data: 50000,
+                img: '../img/item/5.png'
+            }
+        ]
+    },
+    {
+        title: 'Площадка подъёмного механизма',
+        options: [
+            {
+                title: '1040 х 960 мм.',
+                data: 0,
+                img: '../img/item/1.png'
+            },
+            {
+                title: '1250 х 960 мм.',
+                data: 10000,
+                img: '../img/item/2.png'
+            }
+        ]
+    },
+    {
+        title: 'Покрытие площадки подъемного механизма',
+        options: [
+            {
+                title: 'Перфорированный лист',
+                data: 0,
+                img: '../img/item/1.png'
+            },
+            {
+                title: 'Алюминиевый рифленый лист',
+                data: 20000,
+                img: '../img/item/2.png'
+            }
+        ]
+    },
+    {
+        title: 'Скат с площадки подъемного механизма',
+        options: [
+            {
+                title: 'Cтальной (площадка с перфорированным листом)',
+                data: 0,
+                img: '../img/item/1.png'
+            },
+            {
+                title: 'Перфорированный (площадка с перфорированным листом)',
+                data: 0,
+                img: '../img/item/2.png'
+            },
+            {
+                title: 'Алюминиевый (площадка с алюминиевым рифленым листом)',
+                data: 0,
+                img: '../img/item/3.png'
+            },
+        ]
+    },
+    {
+        title: 'Заезд/выезд на площадку подъемного механизма',
+        options: [
+            {
+                title: 'Прямой',
+                data: 0,
+                img: '../img/item/1.png'
+            },
+            {
+                title: 'слева',
+                data: 0,
+                img: '../img/item/2.png'
+            },
+            {
+                title: 'справа',
+                data: 0,
+                img: '../img/item/3.png'
+            },
+            {
+                title: 'прямой и слева',
+                data: 0,
+                img: '../img/item/4.png'
+            },
+            {
+                title: 'прямой и справа',
+                data: 0,
+                img: '../img/item/5.png'
             }
         ]
     },
 ]
 
+
 function updateStep(index) {
-    step.innerHTML = `
-        <div class="item-constuctor__question">${formSteps[index].title}</div>`
+    step.innerHTML = `<div class="item-constuctor__question">${formSteps[index].title}</div>`
     formSteps[index].options.forEach(option => {
-        step.innerHTML += `
-        <a class="btn-light item-constuctor__btn" data-price="${option.data}">${option.title}</a>`
+        step.innerHTML += `<a class="btn-light item-constuctor__btn" data-img=${option.img} data-price="${option.data}">${option.title}</a>`
     })
-    const btns = getBtns()
-    updateBtns(btns)
-    updateControls(btns)
+    updateBtns()
+    updatePoints()
+    updateLine()
 }
 
-function getBtns() {
+function updateBtns() {
     const btns = document.querySelectorAll('.item-constuctor__btn')
-    return btns
-}
-
-function updateBtns(btns) {
-
     btns.forEach(btn => {
         btn.addEventListener('click', () => {
-
             btns.forEach(el => {
                 el.classList.remove('active')
             })
             btn.classList.add('active')
             priceArray[activeStep + 1] = Number(btn.dataset.price)
+            updateImg(btn.dataset.img)
             updatePrice()
-            updateControls()
+            updateControls(btns)
         })
     })
+    updateImg(btns[0].dataset.img)
+    updateControls(btns)
 }
 
 function checkActivatedBtn(btns) {
+    let check = false
     btns.forEach((btn) => {
-        if (btn.classList.contains('active')) return true
+        if (btn.classList.contains('active')) {
+            check = true
+        }
     })
-    return false
+    return check
 }
 
 next.addEventListener('click', (e) => {
@@ -120,12 +411,12 @@ function updateControls(btns) {
 
     if (activeStep === formSteps.length - 1) next.setAttribute('disabled', true)
     if (activeStep < formSteps.length - 1) next.removeAttribute('disabled')
+    if (checkActivatedBtn(btns) && activeStep !== formSteps.length - 1) next.removeAttribute('disabled')
+    if (!checkActivatedBtn(btns)) next.setAttribute('disabled', true)
 
-    if (activeStep === formSteps.length - 1) submitBtn.removeAttribute('disabled')
+    if (activeStep === formSteps.length - 1 && checkActivatedBtn(btns)) submitBtn.removeAttribute('disabled')
     if (activeStep < formSteps.length - 1) submitBtn.setAttribute('disabled', true)
 
-    if (checkActivatedBtn(btns)) next.removeAttribute('disabled')
-    if (!checkActivatedBtn(btns)) next.setAttribute('disabled', true)
 }
 
 function updatePrice() {
@@ -134,6 +425,32 @@ function updatePrice() {
         sum += price;
     }
     price.innerHTML = sum
+}
+
+function updatePoints() {
+    for (let i = 0; i < formSteps.length; i++) {
+        if (i <= activeStep) {
+            pointsArray[i] = true
+        } else {
+            pointsArray[i] = false
+        }
+    }
+    points.innerHTML = ''
+    for (let i = 0; i < formSteps.length; i++) {
+        points.innerHTML += `<div class="item-constuctor__point ${pointsArray[i] ? 'active' : ''}"><span>${i + 1}</span></div>`
+    }
+}
+
+function updateLine() {
+    if (activeStep === formSteps.length - 1) {
+        line.style.width = `99%`
+    } else {
+        line.style.width = `${activeStep / (formSteps.length - 1) * 100}%`
+    }
+}
+
+function updateImg(path) {
+    img.src = path
 }
 
 window.onload = () => {
