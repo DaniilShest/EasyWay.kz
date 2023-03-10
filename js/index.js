71fded3954e2c46f8b58ef8688ad3808fe032d02
Daniil <daniil.shest@gmail.com>
2023-03-10 09:52:10 +0600

    delivery

--- js/index.js
+++ js/index.js
@@ -1,31 +1,4 @@
-const mainSlider = new Swiper('.main-slider', {
-    speed: 500,
-    spaceBetween: 100,
-    autoplay: {
-        delay: 5000,
-    },
-    effect: 'fade',
-    fadeEffect: {
-        crossFade: true
-    },
-    pagination: {
-        el: '.main-slider__pagination',
-        type: 'bullets',
-        clickable: true
-    },
-    autoHeight: true
-});
 
-const mainTrust = new Swiper('.main-trust__slider', {
-    speed: 400,
-    slidesPerView: 4,
-    spaceBetween: 30,
-    pagination: {
-        el: '.main-trust-pagination',
-        type: 'bullets',
-        clickable: true
-    },
-});
 
 const checkbox = document.getElementById("form__checkbox")
 const btn = document.getElementById("form__btn")
@@ -33,5 +6,4 @@ const btn = document.getElementById("form__btn")
 checkbox.addEventListener("change", () => {
     if (btn.hasAttribute('disabled')) { btn.removeAttribute('disabled') }
     else { btn.setAttribute('disabled', true) }
-    console.log(checkbox)
 })
