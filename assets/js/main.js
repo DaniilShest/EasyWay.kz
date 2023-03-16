a141116999fdc78fba8a7c830456535b97bb92d2
Daniil Shestakevich <daniil.shest@gmail.com>
2023-03-16 17:50:34 +0600

    add accordion

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,18 @@ popupCheckbox.addEventListener("change", () => {
     if (popupBtn.hasAttribute('disabled')) { popupBtn.removeAttribute('disabled') }
     else { popupBtn.setAttribute('disabled', true) }
 })
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
