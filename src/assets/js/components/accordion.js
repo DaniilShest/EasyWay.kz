ddb7da2b68fb89f7bd6276334d6ea222609a0bd9
Daniil Shestakevich <daniil.shest@gmail.com>
2023-03-16 17:50:34 +0600

    add accordion

--- src/assets/js/components/accordion.js
+++ src/assets/js/components/accordion.js
@@ -1,12 +1,3 @@
-const popupCheckbox = document.getElementById("popup-checkbox")
-const popupBtn = document.getElementById("popup-btn")
-
-popupCheckbox.addEventListener("change", () => {
-    console.log(popupBtn)
-    if (popupBtn.hasAttribute('disabled')) { popupBtn.removeAttribute('disabled') }
-    else { popupBtn.setAttribute('disabled', true) }
-})
-
 let acc = document.getElementsByClassName("accordion");
 let i;
 
