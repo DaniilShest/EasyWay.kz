17989b038579b62c103c300b2d317954d8904588
Daniil <daniil.shest@gmail.com>
2023-03-16 11:05:44 +0600

    about

--- dist/assets/js/mainSlider.js
+++ dist/assets/js/mainSlider.js
@@ -15,14 +15,3 @@ const mainSlider = new Swiper('.main-slider', {
     },
     autoHeight: true
 });
-
-const mainTrust = new Swiper('.main-trust__slider', {
-    speed: 400,
-    slidesPerView: 4,
-    spaceBetween: 30,
-    pagination: {
-        el: '.main-trust-pagination',
-        type: 'bullets',
-        clickable: true
-    },
-});
---
+++ dist/assets/js/mainTrust.js
@@ -0,0 +1,10 @@
+const mainTrust = new Swiper('.main-trust__slider', {
+    speed: 400,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: '.main-trust-pagination',
+        type: 'bullets',
+        clickable: true
+    },
+});
