12abd99420961481f796a5fc3dd40503d5e8d5a6
Daniil <daniil.shest@gmail.com>
2023-03-19 11:56:25 +0600

    header, catalog, footer and dropdown

--- dist/assets/js/mainTrust.js
+++ dist/assets/js/mainTrust.js
@@ -1,7 +1,7 @@
 const mainTrust = new Swiper('.main-trust__slider', {
     speed: 400,
     slidesPerView: 4,
-    spaceBetween: 30,
+    spaceBetween: 40,
     pagination: {
         el: '.main-trust-pagination',
         type: 'bullets',
