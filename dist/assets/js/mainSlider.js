be6049c529ed03170f079a49dc2e1bf23f21ca5d
Daniil <daniil.shest@gmail.com>
2023-03-27 11:09:54 +0600

    test main page

--- dist/assets/js/mainSlider.js
+++ dist/assets/js/mainSlider.js
@@ -15,3 +15,27 @@ const mainSlider = new Swiper('.main-slider', {
     },
     autoHeight: true
 });
+
+const catalogSlider = new Swiper('.main-catalog__slider', {
+    speed: 500,
+    pagination: {
+        el: '.catalog-slider__pagination',
+        type: 'bullets',
+        clickable: true
+    },
+    autoHeight: true,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+        720: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1060: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    }
+});
