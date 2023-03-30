dcf0a5265e53031bd164ed62550dbfa8ef6434c7
Daniil Shestakevich <daniil.shest@gmail.com>
2023-03-30 14:44:32 +0600

    careers slide

--- assets/js/catalog.js
+++ assets/js/catalog.js
@@ -1,39 +1,40 @@
-document.querySelectorAll('.dropdown').forEach(function (dropdownWrapper) {
-    const dropdownBtn = dropdownWrapper.querySelector('.dropdown__button');
-    const dropdownList = dropdownWrapper.querySelector('.dropdown__list');
-    const dropdownItems = dropdownWrapper.querySelectorAll('.dropdown__list-item');
-    const dropdownInput = dropdownWrapper.querySelector('.dropdown__input_hidden')
 
-    console.log(dropdownBtn)
+document.querySelectorAll('.filter-dropdown').forEach(function (dropdownWrapper) {
+    console.log(document.querySelectorAll('.filter-dropdown'))
+    const dropdownBtn = document.querySelector('.filter-dropdown__button');
+    const dropdownList = document.querySelector('.filter-dropdown__list');
+    const dropdownItems = document.querySelectorAll('.filter-dropdown__list-item');
+    const dropdownInput = document.querySelector('.filter-dropdown__input_hidden')
 
     dropdownBtn.addEventListener('click', function () {
-        dropdownList.classList.toggle('dropdown__list_visible');
-        this.classList.toggle('dropdown__button_active');
+        console.log(dropdownList)
+        dropdownList.classList.toggle('filter-dropdown__list_visible');
+        this.classList.toggle('filter-dropdown__button_active');
     });
 
     dropdownItems.forEach(function (listItem) {
         listItem.addEventListener('click', function (e) {
             dropdownItems.forEach(function (el) {
-                el.classList.remove('dropdown__list-item_active');
+                el.classList.remove('filter-dropdown__list-item_active');
             })
-            e.target.classList.add('dropdown__list-item_active');
+            e.target.classList.add('filter-dropdown__list-item_active');
             dropdownBtn.innerText = this.innerText;
             dropdownInput.value = this.dataset.value;
-            dropdownList.classList.remove('dropdown__list_visible');
+            dropdownList.classList.remove('filter-dropdown__list_visible');
         })
     })
 
     document.addEventListener('click', function (e) {
         if (e.target !== dropdownBtn) {
-            dropdownBtn.classList.remove('dropdown__button_active');
-            dropdownList.classList.remove('dropdown__list_visible');
+            dropdownBtn.classList.remove('filter-dropdown__button_active');
+            dropdownList.classList.remove('filter-dropdown__list_visible');
         }
     })
 
     document.addEventListener('keydown', function (e) {
         if (e.key === 'Tab' || e.key === 'Escape') {
-            dropdownBtn.classList.remove('dropdown__button_active');
-            dropdownList.classList.remove('dropdown__list_visible');
+            dropdownBtn.classList.remove('filter-dropdown__button_active');
+            dropdownList.classList.remove('filter-dropdown__list_visible');
         }
     })
 })
